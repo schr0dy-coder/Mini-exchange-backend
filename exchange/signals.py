aa4503b54f911687b6ef28d58ad33ba2710954d1
Sujal Kumar <sujalkumarsahu24@gmail.com>
2026-02-16 12:12:08 +0530

    trying to fix portfolio

--- exchange/signals.py
+++ exchange/signals.py
@@ -2,11 +2,16 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Portfolio
+from decimal import Decimal
 
 @receiver(post_save, sender=User)
-def create_user_portfolio(sender, instance, created, **kwargs):
+def create_portfolio(sender, instance, created, **kwargs):
     if created:
-        Portfolio.objects.create(user=instance)
+        Portfolio.objects.create(
+            user=instance,
+            available_balance=Decimal("100000"),   # starting capital
+            reserved_balance=Decimal("0"),
+        )
 
 @receiver(post_save, sender=User)
 def save_user_portfolio(sender, instance, **kwargs):

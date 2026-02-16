d00cd8563a7af9f2b1cdfcfb27922b6ad21ba049
Sujal Kumar <sujalkumarsahu24@gmail.com>
2026-02-16 15:10:57 +0530

    market_maker finally being done!! YAY!

--- exchange/signals.py
+++ exchange/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Portfolio
+from .models import Portfolio, Holding, Symbol
 from decimal import Decimal
 
 @receiver(post_save, sender=User)
@@ -12,6 +12,18 @@ def create_portfolio(sender, instance, created, **kwargs):
             available_balance=Decimal("100000"),   # starting capital
             reserved_balance=Decimal("0"),
         )
+        
+        # Seed holdings: give new user 10,000 shares of each symbol
+        symbols = Symbol.objects.all()
+        for symbol in symbols:
+            Holding.objects.get_or_create(
+                user=instance,
+                symbol=symbol,
+                defaults={
+                    "available_quantity": 10000,
+                    "reserved_quantity": 0,
+                }
+            )
 
 @receiver(post_save, sender=User)
 def save_user_portfolio(sender, instance, **kwargs):
